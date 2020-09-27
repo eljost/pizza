6a98e8657f73f0f516d93b17b34c07701f77a968
Johannes Steinmetzer <josteinm@posteo.de>
2020-09-27 18:23:47 +0200

    further work

--- pizza/main.py
+++ pizza/main.py
@@ -1,9 +1,11 @@
 from collections import OrderedDict
 from math import sqrt
+import random
 import sys
 
 import numpy as np
 import pygame as pg
+
 
 
 SIZE = (1000, 1000)
@@ -23,17 +25,13 @@ C_CHEESE = (255, 255, 0)
 C_SALAMI = (165, 8, 8)
 
 # PIZZA
-PIZZA_RADIUS = 300
+PIZZA_RADIUS = 450
 
 # SALAMI
 SALAMI_RADIUS = 35
 
-TOPPINGS = ("SAUCE", "CHEESE", "SALAMI")
-TOPPING_DICT = {
-    "SAUCE": C_SAUCE,
-    "CHEESE": C_CHEESE,
-    "SALAMI": C_SALAMI,
-}
+topping_pngs = ("sauce.png", "salami.png", "cheese.png", "shroom.png", "brokooli.png")
+TOPPINGS = [pg.image.load(png) for png in topping_pngs]
 
 
 def get_pizza():
@@ -51,7 +49,7 @@ def on_menu(pos):
 def on_pizza(pos):
     w, h = pos
     pw, ph = PIZZA_CENTER
-    return sqrt((w-pw)**2 + (h-ph)**2) <= PIZZA_RADIUS
+    return sqrt((w - pw) ** 2 + (h - ph) ** 2) <= PIZZA_RADIUS
 
 
 def pizza_pos(pos):
@@ -88,10 +86,11 @@ def main():
 
     # Setup topping buttons
     topping_buttons = get_topping_buttons()
-    colors = [(i, i, i) for i in range(0, 200, 200//len(topping_buttons))]
+    colors = [(i, i, i) for i in range(0, 200, 200 // len(topping_buttons))]
     menu_surf = pg.Surface((WIDTH, MENU_HEIGHT))
     for i, button in enumerate(topping_buttons):
         pg.draw.rect(menu_surf, colors[i], button)
+        menu_surf.blit(TOPPINGS[i], (button.left, button.top))
 
     pizza = get_pizza()
 
@@ -99,32 +98,41 @@ def main():
         DISPLAY.blit(menu_surf, (0, 0))
         DISPLAY.blit(pizza, PIZZA_TOP_LEFT)
 
-
     cur_topping = 0
-    cur_color = TOPPING_DICT[TOPPINGS[cur_topping]]
+
+    def get_cur_png(rotate=False):
+        cur_png = TOPPINGS[cur_topping].copy()
+        if rotate:
+            cur_png = pg.transform.rotate(cur_png, random.randrange(-180, 181))
+        return cur_png
+    cur_png = get_cur_png()
+
     draw()
     pg.display.update()
     while True:
         event = pg.event.wait()
-        mx, my = pg.mouse.get_pos()
-
 
         if event.type == pg.MOUSEBUTTONDOWN:
             pos = event.pos
             if on_menu(pos):
+                mx, my = pg.mouse.get_pos()
                 for i, button in enumerate(topping_buttons):
                     if button.collidepoint((mx, my)):
                         cur_topping = i
+                        cur_png = get_cur_png(rotate=True)
                         print(f"Selected topping {i}")
+            # Add topping if clicked on pizza
             p_pos = pizza_pos(pos)
             if on_pizza(p_pos):
-                pg.draw.circle(pizza, C_SALAMI, p_pos, SALAMI_RADIUS)
+                print("blit on pizza @", p_pos)
+                pizza.blit(cur_png, p_pos)
+                cur_png = get_cur_png(rotate=True)
             draw()
         elif event.type == pg.MOUSEMOTION:
             pos = event.pos
             draw()
             if not on_menu(pos):
-                pg.draw.circle(DISPLAY, C_SALAMI, pos, SALAMI_RADIUS)
+                DISPLAY.blit(cur_png, pos)
         # Quit
         elif event.type == pg.QUIT:
             quit()
